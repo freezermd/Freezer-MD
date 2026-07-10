3314b5d8348ddda029b21d953e5d804ed14efbf6
Pussy Mêçhàniç <freezermd254@gmail.com>
2026-07-11 00:01:57 +0300

    Add NEWS_API_KEY to config

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   SESSION_DIR: './sessions',
   OWNER_NUMBER: '1234567890',
   PUBLIC_MODE: true,
-
+NEWS_API_KEY: process.env.NEWS_API_KEY || '3cf44e2172724ffa912c56df48c93adc',
   
   FOOTBALL_API_KEY: process.env.FOOTBALL_API_KEY || 'bb30864e187c4791a01c29db90a02b6d'
 };
