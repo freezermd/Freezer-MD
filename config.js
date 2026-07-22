6fc661cac98997174c9c3cace869f66ff7409836
Pussy Mêçhàniç <freezermd254@gmail.com>
2026-07-22 20:14:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   SESSION_DIR: './sessions',
   OWNER_NUMBER: '254791553079',
   PUBLIC_MODE: true,
-  NEWS_API_KEY: process.env.NEWS_API_KEY || '3cf44e2172724ffa912c56df48c93adc',
+  
   FOOTBALL_API_KEY: process.env.FOOTBALL_API_KEY || 'bb30864e187c4791a01c29db90a02b6d',
   BAD_WORDS: ['fuck', 'shit', 'asshole', 'bitch', 'cunt']  // add your list
 };
